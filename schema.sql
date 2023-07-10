67cc1b1da73ca09c0ba11089e9054fe08052e15c
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-07-10 19:51:31 +0300

    Create dev branch

--- schema.sql
+++ schema.sql
@@ -1,10 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-  id int primary key GENERATED ALWAYS AS IDENTITY ,
-  name varchar(100),
-  date_of_birth date,
-  escape_attempts int,
-  neutered boolean,
-  weight_kg decimal
-  );
+    name varchar(100)
+);
