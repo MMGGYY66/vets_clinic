869c5043b3f47eab90997d9142f3b04f801d558b
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-07-12 23:51:55 +0300

    insert data into the owners table

--- data.sql
+++ data.sql
@@ -97,3 +97,13 @@ INSERT INTO animals (
   )
 VALUES ('Ditto', '2022-05-14', 4, true, 22);
 
+-- Day 3 insert data into the owners table
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+  
