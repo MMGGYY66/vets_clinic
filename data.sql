cc06bd50fe07981669438e5133032b0dcf17521e
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-07-13 23:49:55 +0300

    Insert these data for specialties

--- data.sql
+++ data.sql
@@ -163,4 +163,35 @@ VALUES ('William Tatcher', 45, '2000-04-23'),
   ('Stephanie Mendez', 64, '1981-05-04'),
   ('Jack Harkness', 38, '2008-06-08');
 
+-- Insert these data for specialties
+INSERT INTO specializations (species_id, vets_id)
+VALUES (
+    (SELECT id FROM species
+      WHERE name = 'Pokemon'
+    ),
+    (SELECT id FROM vets
+      WHERE name = 'William Tatcher'
+    )
+  ),
+  (
+    (SELECT id FROM species
+      WHERE name = 'Digimon'
+    ),
+    (SELECT id FROM vets
+      WHERE name = 'Stephanie Mendez'
+    )),
+  (
+    (SELECT id FROM species
+      WHERE name = 'Pokemon'
+    ),
+    (SELECT id FROM vets
+      WHERE name = 'Stephanie Mendez'
+    ) ),
+  (
+    (SELECT id FROM species
+      WHERE name = 'Digimon'
+    ),
+    (SELECT id FROM vets
+      WHERE name = 'Jack Harkness'
+    ) );
   
